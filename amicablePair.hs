cb4d27cc9bbfed60d2c16bbad44c35483416798e
Jiayuan Ma <jiayuan.ma@snapchat.com>
2015-10-15 23:58:59 -0700

    minor fix

--- amicablePair.hs
+++ amicablePair.hs
@@ -15,9 +15,10 @@ isAmicable :: (Int, Int) -> Bool
 isAmicable (a, b) = (sumf a == b) && (sumf b == a) 
 
 solve :: Int -> [(Int, Int)]
-solve n = filter isAmicable
+solve n = [ (k1, k3) | (k1, k2) <- cand, (k3, k4) <- cand,
+                       k1 < k3, k1 == k4, k2 == k3 ]
   where
-    cand = [ (x,y) | x<-[1..n], y<-[1..n], x > y ]
+    cand = map (\x -> (x, sumf x)) [1..n]
 
 main :: IO ()
 main = do
